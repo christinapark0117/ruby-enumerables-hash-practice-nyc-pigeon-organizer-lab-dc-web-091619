6f4b378be4d52a11149e7a71c1270ae161d5efbb
Christina Park <christinapark0117@gmail.com>
2019-09-15 20:08:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer (data)
   final = {}
 
-  data.each do |first_level, all_other|
-    all_other.each do |category, array|
+  data.each do |first_level, value|
+    value.each do |category, array|
       array.each do |name|
         final[name] = {:color => [], :gender => [], :lives => []}
       end 
