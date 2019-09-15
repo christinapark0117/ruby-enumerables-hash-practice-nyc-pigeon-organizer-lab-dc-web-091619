ce852368e7d838dc13ea44f6d0079483c2d393fa
Christina Park <christinapark0117@gmail.com>
2019-09-15 19:59:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  def nyc_pigeon_organizer(data)
+	final_hash = {}
+	data.each do |pigeon_characteristics, values|
+		values.each do |value, array|
+			array.each do |name|
+				if final_hash[name] == nil
+					final_hash[name] = {}
+					final_hash[name][pigeon_characteristics] = []
+				else
+					final_hash[name][pigeon_characteristics] = []
+				end
+			end
+		end
+	end	
+	
+final_hash.each do |name, values|
+values.each do |hash_value, array|
+data.each do |pigeon_characteristics, values|
+	values.each do |value, array|
+
+		array.each do |element|
+		if element == name && hash_value == pigeon_characteristics
+
+			final_hash[name][hash_value] << value.to_s
+		end
+	end
+end
+end
+end
+end
+final_hash
+end
 end
